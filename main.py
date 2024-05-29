cb553304a6dcd8f2b6530105bd6b6142485432f5
Aditya <2005akjha@gmail.com>
2024-05-29 23:19:36 +0530

    tried for fun

---
+++ main.py
@@ -0,0 +1,28 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv('GEMINI_API_KEY')
+
+
+genai.configure(
+    api_key = API_KEY
+)
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+instruction = "In this chat , respond as if you're explaining things to a five-year-old child."
+
+while(True):
+    question = input("You: ")
+
+    if(question.strip() == ''):
+        break
+
+    response = chat.send_message(instruction+question)
+    print('\n')
+    print(f"Gemini : {response.text}")
+    print('\n')
+    instruction = ""
